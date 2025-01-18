8168e729b000d52bfad151bdf14b212f16118f1d
Tan Ti Khuu <tak002@edu.zealand.dk>
2025-01-18 15:24:46 +0100

    Add project files.

---
+++ BirdObservationLib/BirdObservation.cs
@@ -0,0 +1,42 @@
+﻿namespace BirdObservationLib
+{
+    public class BirdObservation
+    {
+        public int Id { get; set; }
+        public string Species { get; set; }
+        public int HowMany { get; set; }
+
+        public void ValidateSpecies()
+        {
+            if (Species == null)
+            {
+                throw new ArgumentNullException("Species cannot be null");
+            }
+            if (Species.Length < 2)
+            {
+                throw new ArgumentOutOfRangeException("Species must be at least 2 characters long");
+            }
+        }
+
+        public void ValidateHowMany()
+        {
+            if (HowMany < 0)
+            {
+                throw new ArgumentOutOfRangeException("HowMany must be at least 0");
+            }
+        }
+
+        public void Validate()
+        {
+            ValidateSpecies();
+            ValidateHowMany();
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Species: {Species}, HowMany: {HowMany}";
+        }
+
+
+    }
+}
---
+++ BirdObservationLib/BirdObservationsRepository.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdObservationLib
+{
+    public class BirdObservationsRepository
+    {
+        private List<BirdObservation> _observations; // Liste af BirdObservation-objekter
+        private int _nextId = 1; // Id til næste BirdObservation-objekt
+
+        public BirdObservationsRepository()
+        {
+            _observations = new List<BirdObservation>();
+            {
+                Add(new BirdObservation { Species = "Blåmejse", HowMany = 2 });
+                Add(new BirdObservation { Species = "Ringdue", HowMany = 8 });
+                Add(new BirdObservation { Species = "Musvit", HowMany = 2 });
+                Add(new BirdObservation { Species = "Sølvmåge", HowMany = 12 });
+                Add(new BirdObservation { Species = "Spætmejse", HowMany = 1 });
+            };
+        }
+
+            // returnerer en liste af alle BirdObservation-objekter
+            public List<BirdObservation> GetAll()
+        {
+            return _observations;
+        }
+
+        // returnerer et BirdObservation-objekt med en bestemt Id
+        public BirdObservation? GetById(int id)
+        {
+            return _observations.FirstOrDefault(o => o.Id == id);
+        }
+
+        // Tilføjer et BirdObservation-objekt til listen
+        public BirdObservation Add(BirdObservation observation)
+        {
+            observation.Validate();
+            observation.Id = _nextId++;
+            _observations.Add(observation);
+            return observation;
+        }
+
+        // Sletter et BirdObservation-objekt fra listen
+        public BirdObservation? Delete(int id)
+        {
+            BirdObservation? observation = GetById(id);
+            if (observation == null)
+            {
+                return null;
+            }
+            _observations.Remove(observation);
+            return observation;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
